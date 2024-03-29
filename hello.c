e32fd47513cfdfd6864f4b6fa190ecad3c1221f0
Raghav KR <65363225+RaghavKR17@users.noreply.github.com>
2024-03-29 03:40:32 +0000

    Thu, Mar 28, 2024, 8:40 PM -07:00

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     printf("hello, world\n");
+    string answer = get_string("What's your name? : ");
+    printf("Hello, " + answer);
 }

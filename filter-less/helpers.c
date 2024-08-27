d8cd4714e849bed7e642c32fa7b49dc6b9624b13
Raghav Krishna <65363225+raghavkr17@users.noreply.github.com>
2024-08-27 14:50:37 +0000

    Tue, Aug 27, 2024, 7:50 AM -07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,101 +1,171 @@
 #include "helpers.h"
+#include <math.h> // round function
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //declare variable for average value
+    double avrg;
+
+    //cycle rows
     for (int i = 0; i < height; i++)
     {
+         //cycle columns
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE *pixel = &image[i][j];
-            int avg = round((pixel->rgbtRed + pixel->rgbtGreen + pixel->rgbtBlue) / 3.0);
-            pixel->rgbtRed = avg;
-            pixel->rgbtGreen = avg;
-            pixel->rgbtBlue = avg;
+            //find average value
+            avrg = ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0000);
+
+            //round average value
+            avrg = round(avrg);
+
+            //assign average value to all colors aka change pixels to grayscale
+            image[i][j].rgbtBlue = avrg;
+            image[i][j].rgbtGreen = avrg;
+            image[i][j].rgbtRed = avrg;
         }
     }
+    return;
 }
+
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // declare variables
+    float sepiaRed, sepiaGreen, sepiaBlue;
+    int originalRed, originalGreen, originalBlue;
+
+    //cycle rows
     for (int i = 0; i < height; i++)
     {
+        //cycle columns
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE *pixel = &image[i][j];
-            int red = pixel->rgbtRed;
-            int green = pixel->rgbtGreen;
-            int blue = pixel->rgbtBlue;
+          //assign values to variables
+          originalRed = image[i][j].rgbtRed;
+          originalGreen = image[i][j].rgbtGreen;
+          originalBlue = image[i][j].rgbtBlue;
 
-            int sepiaRed = round(0.393 * red + 0.769 * green + 0.189 * blue);
-            int sepiaGreen = round(0.349 * red + 0.686 * green + 0.168 * blue);
-            int sepiaBlue = round(0.272 * red + 0.534 * green + 0.131 * blue);
+          //used original formula to avoid mistakes or typos.
+          sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+          sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+          sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
 
-            pixel->rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
-            pixel->rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
-            pixel->rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+          //round numbers
+          sepiaRed = round(sepiaRed);
+          sepiaGreen = round(sepiaGreen);
+          sepiaBlue = round(sepiaBlue);
+
+          // check if any value exeed 255, and if so set to 255 else leave as it is
+          sepiaRed = (sepiaRed > 255) ? 255 : sepiaRed;
+          sepiaGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+          sepiaBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+
+          //change oringinal pixels to sepia
+          image[i][j].rgbtRed = sepiaRed;
+          image[i][j].rgbtGreen = sepiaGreen;
+          image[i][j].rgbtBlue = sepiaBlue;
+
         }
     }
+    return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // create temporary storage variable
+    RGBTRIPLE tmp[1][1];
+    int last = width;
+    int first = 0;
+
+
+    //cycle rows
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width / 2; j++)
+        // counters for swaps, reset after each row
+       last = width - 1;
+       first = 0;
+
+        //swap pixels
+        for (int j = 0; j < width/2; j++)
         {
-            // Swap pixel at j with pixel at (width - j - 1)
-            RGBTRIPLE temp = image[i][j];
-            image[i][j] = image[i][width - j - 1];
-            image[i][width - j - 1] = temp;
+            tmp[0][0] = image[i][first];
+            image[i][first]= image[i][last];
+            image[i][last] = tmp[0][0];
+
+            last--;
+            first++;
         }
     }
+    return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Temporary image to hold the blurred result
-    RGBTRIPLE temp[height][width];
+    RGBTRIPLE image_blur[height][width];
+    double avgRed, avgGreen, avgBlue;
+    float counter;
 
+    // cycle rows
     for (int i = 0; i < height; i++)
     {
+        // cycle columns
         for (int j = 0; j < width; j++)
         {
-            int redSum = 0, greenSum = 0, blueSum = 0;
-            int count = 0;
+            counter = 0;
+            avgRed = 0;
+            avgGreen = 0;
+            avgBlue = 0;
 
-            // Iterate over the 3x3 grid around the pixel
-            for (int di = -1; di <= 1; di++)
+            // cycle rows within 9 cell square
+            for (int s = -1; s < 2; s++)
             {
-                for (int dj = -1; dj <= 1; dj++)
+                // cycle columns within 9 cell square
+                for(int n = -1; n < 2; n++)
                 {
-                    int ni = i + di;
-                    int nj = j + dj;
+                    // work only with cells that exist
+                    int d = i + s;
+                    int k = j + n;
 
-                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    if (d < height && k < width && d >= 0 && k >= 0)
                     {
-                        redSum += image[ni][nj].rgbtRed;
-                        greenSum += image[ni][nj].rgbtGreen;
-                        blueSum += image[ni][nj].rgbtBlue;
-                        count++;
+
+                        avgRed = avgRed + image[d][k].rgbtRed;
+                        avgGreen = avgGreen + image[d][k].rgbtGreen;
+                        avgBlue = avgBlue + image[d][k].rgbtBlue;
+
+                        // count existing cells within 9 cell square
+                        counter++;
                     }
                 }
             }
 
-            temp[i][j].rgbtRed = round(redSum / (float)count);
-            temp[i][j].rgbtGreen = round(greenSum / (float)count);
-            temp[i][j].rgbtBlue = round(blueSum / (float)count);
+               // Find average value and round floats to nearest integer
+            avgRed = round(avgRed / counter);
+            avgGreen = round(avgGreen / counter);
+            avgBlue = round(avgBlue / counter);
+
+            // Update blured image array with average values
+            image_blur[i][j].rgbtRed = avgRed;
+            image_blur[i][j].rgbtGreen = avgGreen;
+            image_blur[i][j].rgbtBlue = avgBlue;
+
         }
+
+
     }
 
-    // Copy the blurred image back to the original
+    //overwrite the variable image with blured image
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = temp[i][j];
+            image[i][j] = image_blur[i][j];
         }
     }
+
+    return;
 }

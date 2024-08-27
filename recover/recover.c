d2a039cfc1475c3cb1de04deb632604d1f5de0c0
Raghav Krishna <65363225+raghavkr17@users.noreply.github.com>
2024-08-27 14:55:00 +0000

    Tue, Aug 27, 2024, 7:55 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,8 +1,8 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
     typedef uint8_t BYTE;
@@ -13,8 +13,6 @@ int main (int argc, char *argv[])
 
     FILE *jpg = NULL;
 
-
-
     // Check that two commands entered
     if (argc != 2)
     {
@@ -24,7 +22,7 @@ int main (int argc, char *argv[])
 
     char *pntr_file = argv[1];
 
-    FILE* raw_data = fopen(pntr_file, "r");
+    FILE *raw_data = fopen(pntr_file, "r");
 
     // Check if able to open such file aka exists
     if (raw_data == NULL)
@@ -36,9 +34,7 @@ int main (int argc, char *argv[])
     // check if we have 512 byte chunk
     while (fread(buffer, sizeof(buffer), 1, raw_data) == 1)
     {
-        if (buffer[0] == 0xff &&
-            buffer[1] == 0xd8 &&
-            buffer[2] == 0xff &&
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
 
         {
@@ -46,11 +42,11 @@ int main (int argc, char *argv[])
                 fclose(jpg);
 
             // Create a file name variable
-             char jpg_name[8];
+            char jpg_name[8];
 
             // Name the file
             sprintf(jpg_name, "%03i.jpg", counter);
-            jpg = fopen(jpg_name, "a" );
+            jpg = fopen(jpg_name, "a");
             fwrite(&buffer, sizeof(buffer), 1, jpg);
 
             counter++;
@@ -67,5 +63,4 @@ int main (int argc, char *argv[])
     fclose(raw_data);
     fclose(jpg);
     return 0;
-
 }

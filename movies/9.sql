7e13ebfe3aae375be5229b7492f766c8e88c1178
Raghav Krishna <65363225+raghavkr17@users.noreply.github.com>
2024-08-28 05:32:24 +0000

    Tue, Aug 27, 2024, 10:32 PM -07:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,6 +1,5 @@
-SELECT DISTINCT people.name
-FROM stars
-JOIN people ON stars.person_id = people.id
+SELECT name FROM people
+JOIN stars ON stars.person_id = people.id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
-ORDER BY people.birth;
+ORDER BY people.birth
